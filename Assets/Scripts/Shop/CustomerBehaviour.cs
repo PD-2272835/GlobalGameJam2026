fd75572023c0ff9d7e70822b23e1be8b8f4c861d
PD-2272835 <186196474+PD-2272835@users.noreply.github.com>
2026-01-27 11:35:20 +0000

    Starting work on orders system/telling
    Going for lunch, this is mostly for keeping the main branch up-to-date

--- Assets/Scripts/Shop/CustomerBehaviour.cs
+++ Assets/Scripts/Shop/CustomerBehaviour.cs
@@ -1,15 +1,27 @@
-using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using System.Collections;
 using UnityEngine;
+using TMPro;
 
-public class CustomerBehaviour : IOrderTeller
+public class CustomerBehaviour : MonoBehaviour, IOrderTeller
 {
+    [SerializeField] private string Greeting_;
+    [SerializeField] private string Enquiry_; //"I would like a", "Can I have a"
+    [SerializeField] private Order MaskOrder_;
+
+    public TextMeshProUGUI DialogTMP;
+
+
+
     public void TellOrder()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(ReciteOrder());
     }
 
     private IEnumerator ReciteOrder()
     {
-
+        DialogTMP.text = Greeting_;
+        yield return new WaitForSeconds(0.2f);
+        DialogTMP.text = Enquiry_;
     }
 }
---
+++ Assets/Scripts/Shop/Order.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Order
+{
+    private Mask _Mask;
+    private int _CorrectMaskComponentPrice;
+    private float _TimeLimit;
+
+
+}
